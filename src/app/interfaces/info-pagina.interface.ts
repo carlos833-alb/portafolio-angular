c6417d2a3bb0745c3ed783e46e34fc97e5e4c551
carlos <carloshdez2497@gmail.com>
2020-02-29 23:30:27 -0600

    Servicios Http y animacion

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,10 @@
+export interface infoPagina {
+    titulo?: string;
+    email?: string;
+    nombre_corto?: string;
+    pagina_autor?: string;
+    facebook?: string;
+    twitter?: string;
+    tblr?: string;
+    equipo_trabajo?: any[];
+}
